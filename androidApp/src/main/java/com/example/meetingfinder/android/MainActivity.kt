d333662d5a4a6ba65b9133dfea5605c93d34c1a5
Awais Ahmad <awais.ahmad@cheetay.pk>
2023-11-05 17:33:29 +0500

    Android Compose UI Added

--- androidApp/src/main/java/com/example/meetingfinder/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/meetingfinder/android/MainActivity.kt
@@ -3,38 +3,26 @@ package com.example.meetingfinder.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.meetingfinder.Greeting
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.ui.res.stringResource
+import com.example.meetingfinder.android.ui.MainView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetingView(Greeting().greet())
-                }
+            MainView {
+                TopAppBar(
+                    title = {
+                        when (it) {
+                            0 -> Text(text = stringResource(R.string.world_clocks))
+                            else -> Text(text = stringResource(R.string.findmeeting))
+                        }
+                    }, backgroundColor = MaterialTheme.colors.primary
+                )
             }
         }
-    }
-}
-
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
     }
 }
